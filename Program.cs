26e97e030a8d573a0103463d8546daf44c7043c6
Pavel Yakovlev <hronos2t@gmail.com>
2022-01-14 22:20:47 +0300

    - log - title

--- Program.cs
+++ Program.cs
@@ -2,19 +2,28 @@
 
 var forkName = Helper.GetFork();
 var app = Helper.GetAppPath(forkName);
+var oldTitle = Console.Title ?? string.Empty;
 var phase = "0";
 var progress = 0d;
+var table = "";
 var phaseTime = new TimeSpan();
-var sw = new Stopwatch();
+var totalSw = new Stopwatch();
+var phaseSw = new Stopwatch();
 var isDash = false;
 Process? process = null;
-Tuple<TimeSpan, string> lastOutput = new(TimeSpan.Zero, "");
+
+var d = Directory.CreateDirectory(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs"));
+var streamWriter = new StreamWriter(Path.Combine(d.FullName, DateTime.Now.ToString("yyyy-dd-MM_HH_mm_ss") + ".log"));
+streamWriter.AutoFlush = true;
 
 if (app != null)
 {
-    sw.Restart();
+    totalSw.Restart();    
     Console.CancelKeyPress += OnExit;
     await startJob(app, string.Join(" ", Environment.GetCommandLineArgs().Skip(1).ToArray()));
+    streamWriter.Flush();
+    streamWriter.Close();
+    streamWriter.Dispose();
     OnChiaExit(null, null);
 }
 
@@ -58,25 +67,44 @@ void FilterOutput(string? output)
         return;
     }
 
+    Console.Title = $"{progress}% | elapsed {totalSw.Elapsed.TotalHours:N}h | {oldTitle}";
+
+    streamWriter.WriteLine(output);
+
     var _phase = Helper.GetRegexMatch(@"Starting phase (?<res>\d)\/\d:", output);
     if (_phase != null)
     {
         phase = _phase;
-        writeLine($"-=[ Phase: {phase} ]=-");
+        writeLine($"-=[ Phase: {phase} ]=-         started at {DateTime.Now:HH:mm:ss}");
+        phaseSw.Restart();
         return;
     }
     var _progress = Helper.GetRegexMatch(@"Progress update: (?<res>\d\.\d+)", output);
     if (_progress != null)
     {
         progress = double.Parse(_progress) * 100;
-        writeLine($"[P{phase}] Progress {progress:N}%");
+        writeLine($"[P{phase}] Progress {progress}%");
+        return;
+    }
+    var _table = Helper.GetRegexMatch(@"^Comp\w+ing tables? (?<res>.*)", output);
+    if (_table != null)
+    {
+        table = _table;
+        writeLine($"[P{phase}] Work on table {table}");
+        return;
+    }
+    var _tableTime = Helper.GetRegexMatch(@"table time: (?<res>\d*.\d+) seconds", output);
+    if (_tableTime != null)
+    {
+        var tableTime = TimeSpan.FromSeconds(double.Parse(_tableTime));
+        writeLine($"[P{phase}] Table {table} took {tableTime.TotalMinutes:N}");
         return;
     }
     var _phaseTime = Helper.GetRegexMatch(@"Time for phase \d = (?<res>\d+\.\d+) sec", output);
     if (_phaseTime != null)
     {
         phaseTime = TimeSpan.FromSeconds(double.Parse(_phaseTime));
-        writeLine($"Time of [P{phase}]: {phaseTime}");
+        writeLine($"Phase {phase} took {phaseTime.TotalMinutes:N} minutes");
         return;
     }
 
@@ -100,7 +128,7 @@ void OutputDataReceived(object? sender, DataReceivedEventArgs? e)
 void OnChiaExit(object? sender, EventArgs? e)
 {
     writeLine("Plotter has ended.");
-    writeLine($"Total time: {sw.Elapsed.TotalHours:N} hours");
+    writeLine($"Total time: {totalSw.Elapsed.TotalHours:N} hours");
     Environment.Exit(0);
 }
 
@@ -112,6 +140,6 @@ void OnExit(object? sender, ConsoleCancelEventArgs? e)
         writeLine($"Process {process.ProcessName} | id {process.Id} killed.", ConsoleColor.Yellow);
         process.Kill();
     }
-    writeLine($"Total time: {sw.Elapsed.TotalHours:N} hours");
+    writeLine($"Total time: {totalSw.Elapsed.TotalHours:N} hours");
     Environment.Exit(-1);
 }
